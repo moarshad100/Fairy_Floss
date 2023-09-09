277ff6ec6cd2dd89b3c5cd8b57ae75fd5252787e
moarshad100 <mo.arshad100@gmail.com>
2023-09-09 19:55:18 +1000

    setup made

---
+++ Develop/server.js
@@ -0,0 +1,27 @@
+//Import express.js
+
+const express = require('express');
+
+const path =  require('path');
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
